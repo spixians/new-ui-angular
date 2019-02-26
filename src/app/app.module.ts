4c08b65b0d1ee97582a4f6600d4fdb7383cfced6
jnanesh10 <jnanesh10@gmail.com>
2019-02-27 00:27:35 +0530

    added skeleton of skf UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,6 +13,7 @@ import { RedOrderListComponent } from './skf/red-order-list/red-order-list.compo
 import { GreenOrderListComponent } from './skf/green-order-list/green-order-list.component';
 import { YellowOrderListComponent } from './skf/yellow-order-list/yellow-order-list.component';
 import { AvailablityCheckComponent } from './skf/availablity-check/availablity-check.component';
+import { LoginComponent } from './auth/login/login.component';
 // import { SidebarComponent } from './skf/sidebar/sidebar.component';
 
 @NgModule({
@@ -26,11 +28,13 @@ import { AvailablityCheckComponent } from './skf/availablity-check/availablity-c
     RedOrderListComponent,
     GreenOrderListComponent,
     YellowOrderListComponent,
-    AvailablityCheckComponent
+    AvailablityCheckComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/skf/place-order/place-order.component.ts
+++ src/app/skf/place-order/place-order.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm ,NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-place-order',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlaceOrderComponent implements OnInit {
 
+  @ViewChild('f') placeorder : NgForm
   constructor() { }
 
   ngOnInit() {
   }
+
+  user= {
+    amount: 0,
+    batches: 0,
+    component: "",
+    componentType: "",
+    expectedDate: "",
+    price: 0,
+    quantity: 0,
+    supplier: ""
+
+  }
+
+  // TotalPrice = this.placeorder.value.quantity * this.placeorder.value.price;
+
+  onSubmit(form : NgForm){
+    console.log(this.placeorder)
+    this.user.amount = this.placeorder.value.amount
+    this.user.batches = this.placeorder.value.batches
+    this.user.component= this.placeorder.value.component
+    this.user.componentType= this.placeorder.value.componentType
+    this.user.expectedDate = this.placeorder.value.expectedDate
+    this.user.quantity = this.placeorder.value.quantity
+    this.user.supplier = this.placeorder.value.supplier
+    this.user.price = this.placeorder.value.price
+    console.log(this.user)
+  }
+
 
 }
