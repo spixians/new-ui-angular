3e252dfbec607c55b83bf5c40d5d60be7c00bd9f
palakkeni5 <palakkeni@gmail.com>
2019-02-26 13:05:17 +0530

    added skf sidebar

--- src/app/skf/skf.component.ts
+++ src/app/skf/skf.component.ts
@@ -12,4 +12,33 @@ export class SkfComponent implements OnInit {
   ngOnInit() {
   }
 
+  navbarData: Array<{
+    title: string,
+    icon : string }> =[
+      {
+        title: "Dashboard",
+        icon: "menu-icon fa fa-laptop"
+      },{
+        title:"Demand Estimation",
+        icon : "menu-icon fa fa-bar-chart"
+      },{
+        title:"Place Order",
+        icon : "menu-icon fa fa-tasks"
+      },{
+        title:"Green Orders",
+        icon : "menu-icon fa fa-th"
+      },{
+        title:"Yellow Orders",
+        icon : "menu-icon fa fa-th"
+      },{
+        title:"Red Orders",
+        icon : "menu-icon fa fa-th"
+      },{
+        title:"Availability Check",
+        icon : "menu-icon fa fa-table"
+      },{
+        title:"Order History",
+        icon : "glyphicon glyphicon-list-alt"
+      }
+  ]
 }
