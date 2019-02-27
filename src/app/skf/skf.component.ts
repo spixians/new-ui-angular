50032ed3fa9302df1921067c0f71613a62a24eda
jnanesh10 <jnanesh10@gmail.com>
2019-02-27 12:41:47 +0530

    routing part 1

--- src/app/skf/skf.component.ts
+++ src/app/skf/skf.component.ts
@@ -14,31 +14,40 @@ export class SkfComponent implements OnInit {
 
   navbarData: Array<{
     title: string,
-    icon : string }> =[
+    icon : string,
+    link:string }> =[
       {
         title: "Dashboard",
-        icon: "menu-icon fa fa-laptop"
+        icon: "menu-icon fa fa-laptop",
+        link: "/skf/dashboard"
       },{
         title:"Demand Estimation",
-        icon : "menu-icon fa fa-bar-chart"
+        icon : "menu-icon fa fa-bar-chart",
+        link: "/skf/demand-estimation"
       },{
         title:"Place Order",
-        icon : "menu-icon fa fa-tasks"
+        icon : "menu-icon fa fa-tasks",
+        link: "/skf/place-order"
       },{
         title:"Green Orders",
-        icon : "menu-icon fa fa-th"
+        icon : "menu-icon fa fa-th",
+        link: "/skf/green-order-list"
       },{
         title:"Yellow Orders",
-        icon : "menu-icon fa fa-th"
+        icon : "menu-icon fa fa-th",
+        link: "/skf/yellow-order-list"
       },{
         title:"Red Orders",
-        icon : "menu-icon fa fa-th"
+        icon : "menu-icon fa fa-th",
+        link: "/skf/red-order-list"
       },{
         title:"Availability Check",
-        icon : "menu-icon fa fa-table"
+        icon : "menu-icon fa fa-table",
+        link: "/skf/dashboard"
       },{
         title:"Order History",
-        icon : "menu-icon fa fa-list-alt"
+        icon : "menu-icon fa fa-list-alt",
+        link: "/skf/order-history"
       }
   ]
 }
