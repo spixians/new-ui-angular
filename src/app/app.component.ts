a286b9277499a6d6d56e00cdb6188a74d6411e92
jnanesh10 <jnanesh10@gmail.com>
2019-03-03 16:55:55 +0530

    finalvala

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ export class AppComponent implements OnInit{
   constructor(private router : Router){}
   title = 'new-ui-code';
   ngOnInit(){
-    //  this.router.navigate(['/signin']);
+      this.router.navigate(['/signin']);
     firebase.initializeApp({
       apiKey: "AIzaSyBexhuhh3utOFsA8Yvzqm2yFu1WA5kGzBM",
       authDomain: "skf-sih.firebaseapp.com"
